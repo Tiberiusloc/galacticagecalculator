648e026e55ad6917cf2fcefc650ed51541da7027
Tiberius Lockett <tlockett680@gmail.com>
2022-09-02 09:03:37 -0700

    make test 1 for User

---
+++ __tests__/galacticage.tests.js
@@ -0,0 +1,15 @@
+import {UserAge} from './../src/userAge'
+
+describe ('UserAge', () => {
+  let user;
+  beforeEach(() => {
+    user = new UserAge("Tiberius", "23")
+  });
+  test('should create a new user object with correct properties from UserAge class constructor')
+    expect(user.name).toEqual("Tiberius")
+    expect(user.ageEarth).toEqual('23')
+    expect(user.ageMercury).toBeGreaterThanOrEqualTo(0)
+    expect(user.ageVenus).toBeGreaterThanOrEqualTo(0)
+    expect(user.ageMars).toBeGreaterThanOrEqualTo(0)
+    expect(user.ageJupiter).toBeGreaterThanOrEqualTo(0)
+});
