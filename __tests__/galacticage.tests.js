66414100586669ee6f0a21bd7c148357a54d1a68
Tiberius Lockett <tlockett680@gmail.com>
2022-09-02 11:22:22 -0700

    DRY functions and put math into properties of userage object refactor tests to reflect pass all 6 tests for user age and good fail test 7

--- __tests__/galacticage.tests.js
+++ __tests__/galacticage.tests.js
@@ -8,29 +8,21 @@ describe ('UserAge', () => {
   test('should create a new user object with correct properties from UserAge class constructor', () => { 
     expect(user.name).toEqual("Tiberius");
     expect(user.earth).toEqual(23);
-    expect(user.mercury).toEqual(0);
-    expect(user.venus).toEqual(0);
-    expect(user.mars).toEqual(0);
-    expect(user.jupiter).toEqual(0);
+    expect(user.earthExp).toEqual(80);
   });
   test('should add the different age to user.mercury', () => {
-    user.mercuryAge();
     expect(user.mercury).toBeCloseTo(95.83, 0.1);
   });
   test('should add the different age to user.venus', () => {
-    user.venusAge();
     expect(user.venus).toBeCloseTo(37.09, 0.1);
   });
   test('should add the different age to user.mars', () => {
-    user.marsAge();
     expect(user.mars).toBeCloseTo(12.23, 0.1);
   });
   test('should add the different age to user.jupiter', () => {
-    user.jupiterAge();
     expect(user.jupiter).toBeCloseTo(1.93, 0.1)
   });
   test('should use prototypes to create user age for planets specified in user age object', () => {
-    user.spaceAgeCalculator();
     expect(user.name).toEqual("Tiberius");
     expect(user.earth).toEqual(23);
     expect(user.mercury).toBeCloseTo(95.83, 0.1);
--- src/userAge.js
+++ src/userAge.js
@@ -2,33 +2,14 @@ export class UserAge {
   constructor(name, earth, life) {
     this.name = name
     this.earth = earth
-    this.mercury = 0; 
-    this.venus = 0;
-    this.mars = 0;
-    this.jupiter = 0;
     this.earthExp = life
+    this.mercury = (this.earth / 0.24); 
+    this.venus = (this.earth / 0.62);
+    this.mars = (this.earth / 1.88);
+    this.jupiter = (this.earth / 11.86);
     this.mercuryExp = 0;
     this.venusExp = 0;
     this.marsExp = 0;
     this.jupiterExp = 0;
   }
-  mercuryAge() {
-    this.mercury += (this.earth / 0.24);
-  }
-  venusAge() {
-    this.venus += (this.earth / 0.62);
-  }
-  marsAge() {
-    this.mars += (this.earth / 1.88);
-  }
-  jupiterAge() {
-    this.jupiter += (this.earth / 11.86);
-  }
-  spaceAgeCalculator() {
-    this.mercuryAge();
-    this.venusAge();
-    this.marsAge();
-    this.jupiterAge();
-  }
-
 }
