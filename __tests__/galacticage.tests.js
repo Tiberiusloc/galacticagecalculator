250f0800fc51fcfc9acfb69a86e818cacec5ba79
Tiberius Lockett <tlockett680@gmail.com>
2022-09-02 09:13:56 -0700

    make 2nd test for user age

--- __tests__/galacticage.tests.js
+++ __tests__/galacticage.tests.js
@@ -1,16 +1,20 @@
-import {UserAge} from './../src/userAge'
+import {UserAge} from './../src/userAge';
 
 describe ('UserAge', () => {
   let user;
   beforeEach(() => {
-    user = new UserAge("Tiberius", "23")
+    user = new UserAge("Tiberius", "23");
   });
   test('should create a new user object with correct properties from UserAge class constructor', () => { 
-    expect(user.name).toEqual("Tiberius")
-    expect(user.earth).toEqual('23')
-    expect(user.mercury).toEqual(0)
-    expect(user.venus).toEqual(0)
-    expect(user.mars).toEqual(0)
-    expect(user.jupiter).toEqual(0)
+    expect(user.name).toEqual("Tiberius");
+    expect(user.earth).toEqual('23');
+    expect(user.mercury).toEqual(0);
+    expect(user.venus).toEqual(0);
+    expect(user.mars).toEqual(0);
+    expect(user.jupiter).toEqual(0);
+  });
+  test('should add the different age to user.mercury', () => {
+    user.mercuryAge()
+    expect(user.mercury).toEqual(9.6)
   });
 });
