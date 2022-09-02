c388f5bde441a9c9273ef414a501fcc9dcf660a1
Tiberius Lockett <tlockett680@gmail.com>
2022-09-02 13:32:10 -0700

    refactor life exp test to test 1 for userlife make test 2 for user life

--- __tests__/galacticage.tests.js
+++ __tests__/galacticage.tests.js
@@ -3,13 +3,12 @@ import {UserAge, UserLife} from './../src/userAge';
 describe ('UserAge', () => {
   let user;
   beforeEach(() => {
-    user = new UserAge("Tiberius", 23, 80);
+    user = new UserAge("Tiberius", 23);
     user = new UserLife('Tiberius', 23, 80)
   });
   test('should create a new user object with correct properties from UserAge class constructor', () => { 
     expect(user.name).toEqual("Tiberius");
     expect(user.earth).toEqual(23);
-    expect(user.earthExp).toEqual(80);
   });
   test('should add the different age to user.mercury', () => {
     expect(user.mercury).toBeCloseTo(95.83, 0.1);
@@ -26,10 +25,19 @@ describe ('UserAge', () => {
   test('should calculate life expectancy on mercury and add it to the user.mercuryExp', () => {
     expect(user.mercuryExp).toBeCloseTo(333.33, 0.1);
   });
-  test('should calculate life expectancy on all planets', () => {
-    expect(user.mercuryExp).toBeCloseTo(333.33, 0.1);
-    expect(user.venusExp).toBeCloseTo(129.03, 0.1);
-    expect(user.marsExp).toBeCloseTo(42.55, 0.1);
-    expect(user.jupiterExp).toBeCloseTo(6.74, 0.1);
-  });
 });
+  describe('UserLife', () => {
+    let user;
+    beforeEach(() => {
+      user = new UserLife('Tiberius', 23, 80)
+    });
+    test('should determine the life expectancy of the user for all planets', () => {
+      expect(user.mercuryExp).toBeCloseTo(333.33, 0.1);
+      expect(user.venusExp).toBeCloseTo(129.03, 0.1);
+      expect(user.marsExp).toBeCloseTo(42.55, 0.1);
+      expect(user.jupiterExp).toBeCloseTo(6.74, 0.1);
+    });
+    test('should determine how many years the user has left on mercury', () => {
+      expect(user.mercuryLifeExp()).toBeCloseTo()
+    });
+  });
