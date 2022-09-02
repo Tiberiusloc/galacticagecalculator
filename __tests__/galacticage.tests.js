6d4386e1e35b2d4e12e234b38a87e54f30881b8a
Tiberius Lockett <tlockett680@gmail.com>
2022-09-02 11:13:07 -0700

    refactor all tests and userage prototypes to correct math and expected outcomes

--- __tests__/galacticage.tests.js
+++ __tests__/galacticage.tests.js
@@ -3,11 +3,11 @@ import {UserAge} from './../src/userAge';
 describe ('UserAge', () => {
   let user;
   beforeEach(() => {
-    user = new UserAge("Tiberius", "23");
+    user = new UserAge("Tiberius", 23, 80);
   });
   test('should create a new user object with correct properties from UserAge class constructor', () => { 
     expect(user.name).toEqual("Tiberius");
-    expect(user.earth).toEqual('23');
+    expect(user.earth).toEqual(23);
     expect(user.mercury).toEqual(0);
     expect(user.venus).toEqual(0);
     expect(user.mars).toEqual(0);
@@ -15,28 +15,28 @@ describe ('UserAge', () => {
   });
   test('should add the different age to user.mercury', () => {
     user.mercuryAge();
-    expect(user.mercury).toEqual(5.52);
+    expect(user.mercury).toBeCloseTo(95.83, 0.1);
   });
   test('should add the different age to user.venus', () => {
     user.venusAge();
-    expect(user.venus).toEqual(14.26);
+    expect(user.venus).toBeCloseTo(37.09, 0.1);
   });
   test('should add the different age to user.mars', () => {
     user.marsAge();
-    expect(user.mars).toEqual(43.239999999999995);
+    expect(user.mars).toBeCloseTo(12.23, 0.1);
   });
   test('should add the different age to user.jupiter', () => {
     user.jupiterAge();
-    expect(user.jupiter).toEqual(272.78)
+    expect(user.jupiter).toBeCloseTo(1.93, 0.1)
   });
   test('should use prototypes to create user age for planets specified in user age object', () => {
     user.spaceAgeCalculator();
     expect(user.name).toEqual("Tiberius");
-    expect(user.earth).toEqual("23");
-    expect(user.mercury).toBeCloseTo(5.52, 0.1);
-    expect(user.venus).toBeCloseTo(14.26, 0.1);
-    expect(user.mars).toBeCloseTo(43.24, 0.1);
-    expect(user.jupiter).toBeCloseTo(272.78, 0.1);
+    expect(user.earth).toEqual(23);
+    expect(user.mercury).toBeCloseTo(95.83, 0.1);
+    expect(user.venus).toBeCloseTo(37.09, 0.1);
+    expect(user.mars).toBeCloseTo(12.23, 0.1);
+    expect(user.jupiter).toBeCloseTo(1.93, 0.1);
   });
   test('should calculate life expectancy on mercury and add it to the user.mercuryExp', () => {
 
