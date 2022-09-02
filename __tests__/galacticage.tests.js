aa20aea8d2af4ea1f2a09d7f8863596ae9198387
Tiberius Lockett <tlockett680@gmail.com>
2022-09-02 09:06:27 -0700

    good fail test 1 for userage

--- __tests__/galacticage.tests.js
+++ __tests__/galacticage.tests.js
@@ -5,11 +5,12 @@ describe ('UserAge', () => {
   beforeEach(() => {
     user = new UserAge("Tiberius", "23")
   });
-  test('should create a new user object with correct properties from UserAge class constructor')
-    expect(user.name).toEqual("Tiberius")
-    expect(user.ageEarth).toEqual('23')
-    expect(user.ageMercury).toBeGreaterThanOrEqualTo(0)
-    expect(user.ageVenus).toBeGreaterThanOrEqualTo(0)
-    expect(user.ageMars).toBeGreaterThanOrEqualTo(0)
-    expect(user.ageJupiter).toBeGreaterThanOrEqualTo(0)
+  test('should create a new user object with correct properties from UserAge class constructor', () => { 
+  expect(user.name).toEqual("Tiberius")
+  expect(user.ageEarth).toEqual('23')
+  expect(user.ageMercury).toBeGreaterThanOrEqualTo(0)
+  expect(user.ageVenus).toBeGreaterThanOrEqualTo(0)
+  expect(user.ageMars).toBeGreaterThanOrEqualTo(0)
+  expect(user.ageJupiter).toBeGreaterThanOrEqualTo(0)
+  });
 });
